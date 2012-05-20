c907aeeae569a1800f26d5e1b41f1bc026bcfab5
Antoine d'Otreppe <a.dotreppe@aspyct.org>
2012-05-20 23:33:43 +0200

    Documented ObjectInflator

--- src/Aspyct/MongoMap/ObjectInflator.php
+++ src/Aspyct/MongoMap/ObjectInflator.php
@@ -1,42 +1,86 @@
 <?php
+/*
+ * Copyright (c) 2012 Antoine d'Otreppe de Bouvette <aob@emakina.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
 namespace Aspyct\MongoMap;
 
 require_once __DIR__ . '/Manipulator.php';
 
+/** Array to object converter
+ * 
+ * An object of this class is capable of deserializing data coming
+ * from an ObjectFlattener.
+ * 
+ * First we try to detect the class tag in the given array. If it's found,
+ * an object of this class is created.
+ * 
+ * For each property found in the array, we try to set it on the object.
+ * We try to set the property via three different ways, in the order:
+ *   1. via the corresponding setter, if it exists
+ *   2. set the property directly, if it exists
+ *   3. create a public property on the object
+ *
+ */
 class ObjectInflator extends Manipulator {
     public function inflate(array $data) {
+        // Is this a class as serialized by an ObjectFlattener ?
         if (array_key_exists($this->getClassTag(), $data)) {
+            // Get the class and remove it from the array
             $className = $data[$this->getClassTag()];
             unset($data[$this->getClassTag()]);
             
             $class = new \ReflectionClass($className);
             $object = $class->newInstance();
             
+            // Set the properties of the object
             foreach ($data as $property=>$value) {
                 $setter = 'set'.ucfirst($property);
                 
                 if (is_array($value)) {
+                    // This is probably an object, try to inflate
                     $value = $this->inflate($value);
                 }
                 
+                // There are three ways to set a property
+
+                // 1. via the corresponding setter method
                 if (method_exists($object, $setter)) {
                     $object->$setter($value);
                 }
+                // 2. via the property if it exists
+                else if ($class->hasProperty($property)) {
+                    $accessor = $class->getProperty($property);
+                    $accessor->setAccessible(true);
+                    $accessor->setValue($object, $value);
+                }
+                // 3. set a public property otherwise
                 else {
-                    if ($class->hasProperty($property)) {
-                        $accessor = $class->getProperty($property);
-                        $accessor->setAccessible(true);
-                        $accessor->setValue($object, $value);
-                    }
-                    else {
-                        $object->$property = $value;
-                    }
+                    $object->$property = $value;
                 }
             }
             
             return $object;
         }
         else {
+            // No class detected, return raw array
             return $data;
         }
     }
