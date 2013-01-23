e4841db5e155aebe42b0709506f3c3f3cca7a897
redefinelab <alessandro.perta@gmail.com>
2013-01-23 17:23:56 +0100

    ObjectFlattenerTest refactored to allow nested objects/array + some formatting

--- src/Aspyct/MongoMap/ObjectFlattener.php
+++ src/Aspyct/MongoMap/ObjectFlattener.php
@@ -1,28 +1,51 @@
 <?php
+
 namespace Aspyct\MongoMap;
 
 require_once __DIR__ . '/Manipulator.php';
 
-class ObjectFlattener extends Manipulator {
-    public function flatten($object, $exportClass=true) {
+class ObjectFlattener extends Manipulator
+{
+
+    public function flatten($data, $exportClass = true)
+    {
+
+        // Not an object nor an array, we return the single value
+        if (!is_object($data) && !is_array($data))
+        {
+            return $data;
+        }
+
         $result = array();
-        
-        if ($exportClass) {
-            $result[$this->getClassTag()] = get_class($object);
+
+        // Adding class name to export if asked to and data is an object
+        if ($exportClass && is_object($data))
+        {
+            $result[$this->getClassTag()] = get_class($data);
         }
-        
-        $class = new \ReflectionClass($object);
-        foreach ($class->getProperties() as $property) {
-            $property->setAccessible(true);
-            $value = $property->getValue($object);
-            
-            if (is_object($value)) {
-                $value = $this->flatten($value, $exportClass);
+
+        // If it's an object, we determine properties and recurse
+        if (is_object($data))
+        {
+
+            $class = new \ReflectionClass($data);
+            foreach ($class->getProperties() as $property)
+            {
+                $property->setAccessible(true);
+                $result[$property->getName()] = $this->flatten($property->getValue($data));
             }
-            
-            $result[$property->getName()] = $value;
         }
-        
+        // It's an array
+        else
+        {
+            foreach ($data as $key => $value)
+            {
+                $result[$key] = $this->flatten($value);
+            }
+        }
+
+
         return $result;
     }
+
 }
